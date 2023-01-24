410a3c328b2a87fffb7c542e4a051015dda37bd3
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2023-01-23 19:38:10 -0800

    added mui and styled components

---
+++ utils/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+ palette: {
+    primary: {
+        light: '#F3F3F0',
+        main: '#DD727F',
+        contrastText: '#fff',
+      },
+ },
+});
