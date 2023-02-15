9cf5546ed0b86b19bb529633560781e7edc8316b
Jason Kwak <jkwak10my.bcit.ca>
2023-02-15 00:12:08 -0800

    added cursor and revised pill menu

--- components/pillmenu/pillMenu.js
+++ components/pillmenu/pillMenu.js
@@ -3,37 +3,35 @@ import Button from "@mui/material/Button"
 import styles from "../../styles/Home.module.css";
 import styled from 'styled-components';
 import { Typography } from "@mui/material";
-
 const Wrapper = styled.div`
 `
-
 const Pill = styled.div`
-background-color:var(--sand-color);
+background-color:white;
 border-radius:20px;
 text-align:center;
-border: 1px solid #29252D;
+border: 1px solid var(--gray-color);
 width:100%;
 padding: .5em 1.25em;
 margin: 0.5em 0;
 white-space: nowrap;
-color:var(--jetblack-color);
+color:var(--gray-color);
+box-shadow: rgba(0, 0, 0, 0.25) 0px 1px 1px;
 &:hover{
     background-color:var(--jetblack-color);
-    transition: 0.5s ease;
+    transition: 0.25s ease-in-out;
     color:var(--sand-color);
+    border: 1px solid var(--jetblack-color);
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+    margin: 0.25em 0;
 }
 `
-
 const PillText = styled(Typography)`
-
 `
-
 
 export default function PillMenu({
     onPill=()=>{},
     category='Sample Category'
 }){
-
     return(
         <Wrapper>
         <Pill onClick={onPill}>
